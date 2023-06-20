775886aa4f8dea551f3ca2889c1a4302bb73883f
Satyam Thakur <sat.thakur20@gmail.com>
2023-06-20 11:40:28 +0530

    Repo init with gradle dependencies

---
+++ app/src/main/java/com/satyamthakur/learning/retrofitwithcoroutines/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.satyamthakur.learning.retrofitwithcoroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
